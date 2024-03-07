dfb65240bc406af0d76504fed94e682344b43fcf
Jan Kallwies <jan@kallwies.com>
2024-03-07 23:08:44 +0100

    Implement first working version

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,142 @@
+use egui::debug_text::print;
 use egui::*;
 use std::collections::HashMap;
 use std::vec::Vec;
 
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct ColumnConfig {
+    id: String,
     caption: String,
     unit: String,
 }
 
+enum Action {
+    Remove,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct RowData {
+    name: String,
+
     col_data: HashMap<String, f64>,
-    // sum_col_data: HashMap<String, f64>,
     children: Vec<RowData>,
+
+    // UI State
+    expanded: bool,
+    edit_name: bool,
+}
+
+impl RowData {
+    fn update(&mut self, column_configs: &Vec<ColumnConfig>) {
+        // Update all children
+        for child in self.children.iter_mut() {
+            child.update(column_configs);
+        }
+
+        // Update the actual values
+        for col_cfg in column_configs.iter() {
+            let col_id = &col_cfg.id;
+
+            if !self.children.is_empty() {
+                let mut sum = 0.0;
+                for child in self.children.iter() {
+                    sum += child.col_data.get(col_id).unwrap_or(&0.0);
+                }
+                self.col_data.insert(col_id.clone(), sum);
+            }
+        }
+    }
+
+    fn render(
+        &mut self,
+        ui: &mut Ui,
+        column_configs: &Vec<ColumnConfig>,
+        indent_level: i32,
+    ) -> Option<Action> {
+        let mut action = None;
+
+        ui.horizontal(|ui| {
+            ui.add_space(10.0 * indent_level as f32);
+            ui.expand_button(&mut self.expanded);
+            if self.edit_name {
+                if ui.text_edit_singleline(&mut self.name).lost_focus() {
+                    if !self.name.is_empty() {
+                        self.edit_name = false;
+                    }
+                }
+            } else {
+                if ui.label(self.name.clone() + ":").double_clicked() {
+                    self.edit_name = true;
+                }
+            }
+        });
+
+        let leaf_node = self.children.is_empty();
+
+        for col_cfg in column_configs.iter() {
+            let col_id = &col_cfg.id;
+            let value = *self.col_data.get(col_id).unwrap_or(&0.0);
+            let unit = col_cfg.unit.clone();
+
+            ui.add_space(10.0 * indent_level as f32);
+            if leaf_node {
+                ui.add(
+                    egui::DragValue::new(self.col_data.get_mut(col_id).unwrap())
+                        .speed(1.0)
+                        .suffix(format!(" {unit}")),
+                );
+            } else {
+                ui.label(format!("{value} {unit}"));
+            }
+        }
+        if ui.button("🗑").clicked() {
+            action = Some(Action::Remove);
+        }
+        ui.end_row();
+
+        // Optionally add the children
+        if self.expanded {
+            let mut remove_idx = None;
+            for (i, child) in self.children.iter_mut().enumerate() {
+                match child.render(ui, column_configs, indent_level + 1) {
+                    Some(Action::Remove) => remove_idx = Some(i),
+                    None => (),
+                }
+            }
+
+            if let Some(i) = remove_idx {
+                self.children.remove(i);
+            }
+
+            // Button to add a new element at the same level
+            ui.horizontal(|ui| {
+                ui.add_space(10.0 * (indent_level + 1) as f32);
+                if ui.button("+").clicked() {
+                    let mut new_col_data = HashMap::new();
+                    for col_cfg in column_configs.iter() {
+                        if self.children.is_empty() {
+                            new_col_data.insert(
+                                col_cfg.id.clone(),
+                                *self.col_data.get(&col_cfg.id).unwrap_or(&0.0),
+                            );
+                        } else {
+                            new_col_data.insert(col_cfg.id.clone(), 0.0);
+                        }
+                    }
+                    self.children.push(RowData {
+                        name: "".to_owned(),
+                        col_data: new_col_data,
+                        children: vec![],
+                        expanded: false,
+                        edit_name: true,
+                    });
+                }
+            });
+            ui.end_row();
+        }
+
+        return action;
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -20,54 +146,86 @@ pub struct RowData {
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TreeTablesApp {
     #[serde(skip)]
-    column_config: HashMap<String, ColumnConfig>,
+    column_configs: Vec<ColumnConfig>,
 
-    #[serde(skip)]
     root_row: RowData,
-
-    // Example stuff
-    dummy_bool: bool,
-    dummy_usize: usize,
-    checklist: [bool; 3],
-    num_columns: usize,
-    expanded: [bool; 7],
-    price: [f32; 5],
-    hours: [f32; 5],
 }
 
 impl Default for TreeTablesApp {
     fn default() -> Self {
         Self {
-            column_config: HashMap::from([
-                (
-                    "A".to_owned(),
-                    ColumnConfig {
-                        caption: "foo".to_owned(),
-                        unit: " h".to_owned(),
-                    },
-                ),
-                (
-                    "B".to_owned(),
-                    ColumnConfig {
-                        caption: "bar".to_owned(),
-                        unit: " €".to_owned(),
-                    },
-                ),
-            ]),
+            column_configs: vec![
+                ColumnConfig {
+                    id: "cost".to_owned(),
+                    caption: "Materialkosten".to_owned(),
+                    unit: "€".to_owned(),
+                },
+                ColumnConfig {
+                    id: "hours".to_owned(),
+                    caption: "Arbeitszeit".to_owned(),
+                    unit: "h".to_owned(),
+                },
+            ],
 
             root_row: RowData {
-                col_data: HashMap::from([]),
-                children: vec![],
+                name: "∑".to_owned(),
+                col_data: HashMap::from([("hours".to_owned(), 5.0), ("cost".to_owned(), 10.0)]),
+                children: vec![
+                    RowData {
+                        name: "A".to_owned(),
+                        col_data: HashMap::from([
+                            ("hours".to_owned(), 1.0),
+                            ("cost".to_owned(), 8.0),
+                        ]),
+                        children: vec![
+                            RowData {
+                                name: "a".to_owned(),
+                                col_data: HashMap::from([
+                                    ("hours".to_owned(), 1.0),
+                                    ("cost".to_owned(), 8.0),
+                                ]),
+                                children: vec![],
+                                expanded: false,
+                                edit_name: false,
+                            },
+                            RowData {
+                                name: "b".to_owned(),
+                                col_data: HashMap::from([
+                                    ("hours".to_owned(), 1.0),
+                                    ("cost".to_owned(), 8.0),
+                                ]),
+                                children: vec![],
+                                expanded: false,
+                                edit_name: false,
+                            },
+                            RowData {
+                                name: "c".to_owned(),
+                                col_data: HashMap::from([
+                                    ("hours".to_owned(), 1.0),
+                                    ("cost".to_owned(), 8.0),
+                                ]),
+                                children: vec![],
+                                expanded: false,
+                                edit_name: false,
+                            },
+                        ],
+                        expanded: false,
+                        edit_name: false,
+                    },
+                    RowData {
+                        name: "B".to_owned(),
+                        col_data: HashMap::from([
+                            ("hours".to_owned(), 1.0),
+                            ("cost".to_owned(), 8.0),
+                        ]),
+                        children: vec![],
+                        expanded: false,
+                        edit_name: false,
+                    },
+                ],
+                expanded: false,
+                edit_name: false,
             },
-
-            // Example stuff:
-            expanded: [false, false, false, false, false, false, false],
-            dummy_bool: false,
-            dummy_usize: 10,
-            checklist: std::array::from_fn(|i| i == 0),
-            num_columns: 3,
-            price: [0.0, 42.0, 0.0, 0.0, 12.0],
-            hours: [0.0, 42.0, 0.0, 0.0, 12.0],
         }
     }
 }
@@ -116,6 +274,8 @@ impl eframe::App for TreeTablesApp {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
+        self.root_row.update(&self.column_configs);
+
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
@@ -140,10 +300,15 @@ impl eframe::App for TreeTablesApp {
                 // The central panel the region left after adding TopPanel's and SidePanel's
                 ui.heading("Tree Tables");
 
-                egui::Grid::new("some_unique_id").show(ui, |ui| {
+                egui::Grid::new("table").show(ui, |ui| {
                     ui.label("");
-                    ui.label("Materialkosten (€)");
-                    ui.label("Arbeitszeit (Std.)");
+
+                    // HEADLINE
+                    for cfg in self.column_configs.iter() {
+                        let caption = &cfg.caption;
+                        let unit = &cfg.unit;
+                        ui.label(format!("{caption} ({unit})"));
+                    }
                     ui.horizontal(|ui| {
                         ui.add_space(20.0);
                         if ui.button("+").clicked() {
@@ -152,203 +317,7 @@ impl eframe::App for TreeTablesApp {
                     });
                     ui.end_row();
 
-                    ui.horizontal(|ui| {
-                        ui.expand_button(&mut self.expanded[0]);
-                        ui.label("∑:");
-                    });
-                    let price_sum = self.price[0]
-                        + self.price[1]
-                        + self.price[2]
-                        + self.price[3]
-                        + self.price[4];
-                    let hours_sum = self.hours[0]
-                        + self.hours[1]
-                        + self.hours[2]
-                        + self.hours[3]
-                        + self.hours[4];
-                    ui.label(format!("{price_sum} €"));
-                    ui.label(format!("{hours_sum} Std."));
-                    ui.end_row();
-
-                    if self.expanded[0] {
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.expand_button(&mut self.expanded[1]);
-                            ui.label("Foo:");
-                        });
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            let sum = self.price[0] + self.price[1] + self.price[2];
-                            ui.label(format!("{sum} €"));
-                        });
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            let sum = self.hours[0] + self.hours[1] + self.hours[2];
-                            ui.label(format!("{sum} Std."));
-                        });
-                        if ui.button("🗑").clicked() {
-                            //TOOD: Remove element!
-                        }
-                        ui.end_row();
-
-                        if self.expanded[1] {
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.expand_button(&mut self.expanded[2]);
-                                ui.label("C:");
-                            });
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.add(
-                                    egui::DragValue::new(&mut self.price[0])
-                                        .speed(1.0)
-                                        .suffix(" €"),
-                                );
-                            });
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.add(
-                                    egui::DragValue::new(&mut self.hours[0])
-                                        .speed(1.0)
-                                        .suffix(" Std."),
-                                );
-                            });
-                            if ui.button("🗑").clicked() {
-                                //TOOD: Remove element!
-                            }
-                            ui.end_row();
-
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.expand_button(&mut self.expanded[3]);
-                                ui.label("Rust:");
-                            });
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.add(
-                                    egui::DragValue::new(&mut self.price[1])
-                                        .speed(1.0)
-                                        .suffix(" €"),
-                                );
-                            });
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.add(
-                                    egui::DragValue::new(&mut self.hours[1])
-                                        .speed(1.0)
-                                        .suffix(" Std."),
-                                );
-                            });
-                            if ui.button("🗑").clicked() {
-                                //TOOD: Remove element!
-                            }
-                            ui.end_row();
-
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.expand_button(&mut self.expanded[4]);
-                                ui.label("C++:");
-                            });
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.add(
-                                    egui::DragValue::new(&mut self.price[2])
-                                        .speed(1.0)
-                                        .suffix(" €"),
-                                );
-                            });
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                ui.add(
-                                    egui::DragValue::new(&mut self.hours[2])
-                                        .speed(1.0)
-                                        .suffix(" Std."),
-                                );
-                            });
-                            if ui.button("🗑").clicked() {
-                                //TOOD: Remove element!
-                            }
-                            ui.end_row();
-
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                if ui.button("+").clicked() {
-                                    // TODO: Add new element at same level!
-                                }
-                            });
-                            ui.end_row();
-                        }
-
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.expand_button(&mut self.expanded[5]);
-                            ui.label("Java:");
-                        });
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.add(
-                                egui::DragValue::new(&mut self.price[3])
-                                    .speed(1.0)
-                                    .suffix(" €"),
-                            );
-                        });
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.add(
-                                egui::DragValue::new(&mut self.hours[3])
-                                    .speed(1.0)
-                                    .suffix(" Std."),
-                            );
-                        });
-                        if ui.button("🗑").clicked() {
-                            //TOOD: Remove element!
-                        }
-                        ui.end_row();
-
-                        if self.expanded[5] {
-                            ui.horizontal(|ui| {
-                                ui.add_space(20.0);
-                                if ui.button("+").clicked() {
-                                    // TODO: Add new deeper element!
-                                }
-                            });
-                            ui.end_row();
-                        }
-
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.expand_button(&mut self.expanded[6]);
-                            ui.label("JavaScript:");
-                        });
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.add(
-                                egui::DragValue::new(&mut self.price[4])
-                                    .speed(1.0)
-                                    .suffix(" €"),
-                            );
-                        });
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            ui.add(
-                                egui::DragValue::new(&mut self.hours[4])
-                                    .speed(1.0)
-                                    .suffix(" Std."),
-                            );
-                        });
-                        if ui.button("🗑").clicked() {
-                            //TOOD: Remove element!
-                        }
-                        ui.end_row();
-
-                        ui.horizontal(|ui| {
-                            ui.add_space(10.0);
-                            if ui.button("+").clicked() {
-                                // TODO: Add new element at same level!
-                            }
-                        });
-                        ui.end_row();
-                    }
+                    self.root_row.render(ui, &self.column_configs, 0);
                 });
 
                 ui.separator();
