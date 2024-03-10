a1fa41ca162b2378a943c3e86b07730f2bab0256
Jan Kallwies <jan@kallwies.com>
2024-03-10 20:07:45 +0100

    Fix / update the window title

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Tree-Tables",
         native_options,
         Box::new(|cc| Box::new(tree_tables::TreeTablesApp::new(cc))),
     )
